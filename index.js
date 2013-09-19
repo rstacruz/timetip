0d9baed4ae8e23b47f4875b6421fb60a58818ce9
Rico Sta. Cruz <hi@ricostacruz.com>
2013-09-19 22:22:47 +0800

    Json reporter.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   TimeLog: require('./lib/time_log'),
   reporters: {
-    'default': require('./lib/reporters/default')
+    'default': require('./lib/reporters/default'),
+    'json': require('./lib/reporters/json')
   }
 };

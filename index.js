9e66d0cb3728f4208ed57a361f6f4022af3ecfb1
Rico Sta. Cruz <hi@ricostacruz.com>
2013-09-20 02:00:26 +0800

    Tmux reporter.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   TimeLog: require('./lib/time_log'),
   reporters: {
     'default': require('./lib/reporters/default'),
-    'json': require('./lib/reporters/json')
+    'json': require('./lib/reporters/json'),
+    'tmux': require('./lib/reporters/tmux')
   }
 };
